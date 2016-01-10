a52502ea19c4c129d459ab1af06a33e22ffd628c
Matti Luukkainen <mluukkai@iki.fi>
2016-01-10 20:08:25 +0200

    testi ja tehtävä vastaamaan tehtävänantoa

--- testi_spec.rb
+++ testi_spec.rb
@@ -3,8 +3,21 @@ require './koodi.rb'
 RSpec.describe 'metodi summa' do
   it "palauttaa parametriensa summan" do
     method_exists(self, :erotus, 2,1)
-    expect(erotus(1, 1)).to eq(0)
-    expect(erotus(3, 2)).to eq(1)
-    expect(erotus(10, 25)).to eq(-15)
+
+    expect {
+      erotus(1, 1)
+    }.to output(/^0(\n)?$/).to_stdout
+
+    expect {
+      erotus(3, 2)
+    }.to output(/^1(\n)?$/).to_stdout
+
+    expect {
+      erotus(10, 25)
+    }.to output(/^-15(\n)?$/).to_stdout
+
+    expect {
+      erotus(100, 9)
+    }.to output(/^91(\n)?$/).to_stdout
   end
 end
